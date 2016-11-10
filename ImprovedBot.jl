1a2fb6e333b992022b6a1922696801c840d8b5a9
Kai Xu <xukai921110@gmail.com>
2016-11-10 16:44:49 +0000

    Improve bot

--- ImprovedBot.jl
+++ ImprovedBot.jl
@@ -11,6 +11,13 @@ while true
     for x in 0:gameMap.width-1
       if getSite(gameMap, Location(x, y)).owner == myID
         movedPiece = false
+        for d in CARDINALS
+          if getSite(gameMap, Location(x, y), d).owner != myID && getSite(gameMap, Location(x, y), d).strength < getSite(gameMap, Location(x, y)).strength
+            push!(moves, Move(Location(x, y), d))
+            movedPiece = true
+            break
+          end
+        end
         if ~movedPiece && getSite(gameMap, Location(x, y)).strength < getSite(gameMap, Location(x, y)).production * 5
           push!(moves, Move(Location(x, y), STILL))
           movedPiece = true
